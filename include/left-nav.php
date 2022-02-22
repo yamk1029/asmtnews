b86c79c4e88049eba05d40934d875b0b8a19d6ab
bidhan <yamk1029@gmail.com>
2022-02-22 14:09:07 +0545

    first commit

---
+++ include/left-nav.php
@@ -0,0 +1,8 @@
+<div class="col-4">
+        <ul class="list-group">
+          <li class="list-group-item"><a href="category.php">Add Category</a></li>
+          <li class="list-group-item"><a href="add-post.php">Add Post</a></li>
+          <li class="list-group-item"><a href="post.php">Add Post</a></li>
+        </ul>
+</div>
+
